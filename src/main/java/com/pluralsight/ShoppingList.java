9326e4824311ffd26def27d2d7337d05098b1aa0
sharianakram <sharianakram0503@gmail.com>
2024-09-25 15:13:57 -0400

    This is my shopping list

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        System.out.println("Shopping List");
+        System.out.println("-----------------------");
+
+        System.out.println("apple");
+        System.out.println("bananas");
+        System.out.println("carrots");
+        System.out.println("dragon fruit");
+        System.out.println("cheese");
+        System.out.println("milk");
+        System.out.println("ice cream");
+        System.out.println("chicken");
+        System.out.println("steak");
+        System.out.println("broccoli");
+
+    }
+
+}
